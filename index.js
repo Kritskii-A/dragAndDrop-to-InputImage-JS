4a41d6cf76a4ff083ba6d88e7645299c5a9dd9f9
Kritskii-A <kritskii-a@bk.ru>
2020-10-08 15:20:57 +0300

    v 0.1

---
+++ index.js
@@ -0,0 +1,48 @@
+/* DRAG and Drop */
+function dragAndDropImage(input) {
+  if (input === "reset") {
+    let inputImage = document.querySelector("#imagefile");
+    inputImage.value = "";
+  } else {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        var image = new Image();
+        image.onload = function () {
+          if (this.width >= 1080 || this.height >= 1080) {
+            $(".errorUploadImage").html("Слишком большое изображение");
+            removeUploadImg("true");
+          } else {
+            $(".imageUpload--wrap").hide();
+
+            $(".fileUpload--content").attr(
+              "style",
+              "background-image: url(" + e.target.result + ")"
+            );
+            $(".fileUpload--content").show();
+            $(".errorUploadImage").html("");
+          }
+        };
+        image.src = e.target.result;
+      };
+
+      reader.readAsDataURL(input.files[0]);
+    } else {
+      removeUploadImg();
+    }
+  }
+}
+
+function removeUploadImg(info) {
+  $(".fileUpload--input").replaceWith($(".fileUpload--input").clone());
+  $(".fileUpload--content").hide();
+  $(".imageUpload--wrap").show();
+  if (!info) {
+    $(".errorUploadImage").html("");
+  }
+
+  dragAndDropImage("reset");
+}
+
+/* END DRAG and Drop */
